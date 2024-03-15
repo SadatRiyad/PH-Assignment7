ae9ac3a059a821b46c7d7698c841e6b226881d3d
SadatRiyad <sadatriyad@students.diu.ac>
2024-03-16 04:51:58 +0600

    navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
+
     <>
-      <h1>BB-Food</h1> 
+    <Navbar></Navbar>
+  
     </>
   )
 }
 
 export default App
 
+
+
+
+// const [count, setCount] = useState(0)
 
 {/* <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#150B2B",
+        secondary: "#150B2B99",
+        tertiary: "#878787",
+        "p-color": "#FFFFFF",
+        green: "#0BE58A",
+      },
+    },
   },
+  // eslint-disable-next-line no-undef
   plugins: [require("daisyui")],
-}
-
+};
